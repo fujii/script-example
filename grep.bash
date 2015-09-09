98fbb2c712dc3ac8d7567eb6c7027c00acd87cf1
Fujii Hironori <fujii.hironori@gmail.com>
2015-09-10 00:14:42 +0900

    Added grep.bash

---
+++ grep.bash
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+usage_exit() {
+    echo "Usage: $0 [options] PATTERN ..." 1>&2
+    echo "Options:" 1>&2
+    echo "  -r: recursive" 1>&2
+    exit 1
+}
+
+while getopts r:h OPT
+do
+    case $OPT in
+        r)  recursive=1
+            ;;
+        h)  usage_exit
+            ;;
+        \?) usage_exit
+            ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+if [[ $# -lt 1 ]]; then
+   usage_exit
+fi
+
+pattern="$1"
+shift
+
+for file in "$@";do
+    while read line;do
+	if [[ "$line" =~ "$pattern" ]];then
+	    echo "$file:$line"
+	fi
+    done < "$file"
+done
